47b850b67364d09405c4bdd0a480f05ef5c63022
Kelep <gunawan.fredy97@gmail.com>
2019-04-07 14:28:06 +0700

    Create new model and controller
    create Post model and controller model for manage post on blog

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-class Pages extends Controller{
+class PagesController extends Controller{
 
     public function getIndex(){
         #process variable & params
