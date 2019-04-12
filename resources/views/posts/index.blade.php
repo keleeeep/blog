fc34f2aff67a84139cbecdc377ae9eba2c97a98c
Kelep <gunawan.fredy97@gmail.com>
2019-04-13 01:13:25 +0700

    Add PostController and View

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,41 @@
+@extends('main')
+
+@section('title','| All Posts')
+
+@section('content')
+<div class="row">
+    <div class="col-md-10">
+        <h1>All Posts</h1>
+    </div>
+    <div class="col-md-2">
+    <a href="{{route('posts.create')}}" class="btn btn-primary btn-block mt-2">Create Post</a>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+        <table class="table">
+            <thead>
+                <th>#</th>
+                <th>Title</th>
+                <th>Body</th>
+                <th>Created At</th>
+                <th>Action</th>
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                    <tr>
+                        <th class="align-middle"> {{$post->id}} </th>
+                        <td class="align-middle"> {{$post->title}} </td>
+                        <td class="align-middle"> {{ substr($post->body,0,50) }} {{strlen($post->body) > 50 ? "..." : ""}} </td>
+                        <td class="align-middle"> {{date('M j, Y',strtotime($post->created_at))}} </td>
+                        <td class="align-middle"s>
+                            <a href="{{route('posts.show',$post->id)}}" class="btn btn-outline-dark btn-sm">View</a>
+                            <a href="{{route('posts.edit',$post->id)}}" class="btn btn-outline-dark btn-sm">Edit</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,33 @@
+@extends('main')
+
+@section('title','| Show Post')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-8">
+            <h1> {{$post->title}} </h1>
+            <p> {{$post->body}} </p>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <p class="card-text mb-0"><strong>Created at</strong></p>
+                    <p>{{date('M j, Y h:i a',strtotime($post->created_at))}}</p>
+                    <p class="cart-text mb-0"><strong>Updated at</strong></p>
+                    <p>{{date('M j, Y h:i a',strtotime($post->updated_at))}}</p>
+                    <hr>
+                    <div class="row">
+                        <div class="col-6">
+                            {!! Html::linkRoute('posts.edit','Edit',array($post->id), array('class'=>'btn btn-primary btn-block')) !!}
+                        </div>
+                        <div class="col-6">
+                            {!! Form::open(['route'=>['posts.destroy',$post->id],'method'=>'DELETE']) !!}
+                            {!! Form::submit('Delete',['class'=>'btn btn-danger btn-block'])  !!}
+                            {!! Form::close() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
