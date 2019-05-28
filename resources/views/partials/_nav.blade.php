3daef01d48a10155e3e871039f074d6bf3feba63
Kelep <gunawan.fredy97@gmail.com>
2019-05-28 13:19:45 +0700

    Dynamic Navbar

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -32,20 +32,20 @@
     </div>
 </nav> --}}
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
   <a class="navbar-brand" href="#">Laravel</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
+        <li class="nav-item {{Request::is('/') ? "active" : ""}} ">
+            <a class="nav-link" href="/">Home</a>
       </li>
-      <li class="nav-item">
+        <li class="nav-item {{Request::is('about') ? "active" : ""}} ">
         <a class="nav-link" href="about">About</a>
       </li>
-      <li class="nav-item">
+        <li class="nav-item {{Request::is('contact') ? "active" : ""}}">
         <a class="nav-link" href="contact">Contact</a>
       </li>
     </ul>
